1c1300211d18acae27862fdd562d73360371f662
BurningButter <jjh5@protonmail.com>
2024-11-12 13:38:07 -0500

    I'm so tired of this

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-"https://foutaindatagrabber.vercel.app/api/data"
+async function getData() {
+  const url = "https://foutaindatagrabber.vercel.app/api/data";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
