bdddd74a40486a6a0f01e5dd8e0c1b9f9de6a1df
BurningButter <jjh5@protonmail.com>
2024-11-14 11:31:45 -0500

    Alright, clientside time

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-async function getData() {
-  const url = "https://foutaindatagrabber.vercel.app/api/data";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
+const json = Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vS5iZwKtAoTtOLa-arMPvBVQAVP0tpraL2PTZr2eopwHfsHhZ_9_7XqlbAmwDVvJqnA-TZbfX_PvQRd/pub?output=csv", {
+  download: true,
+  delimiter: ",",
+  header: true,
+  complete: function (results) {
+    console.log(results);
   }
-}
+});
